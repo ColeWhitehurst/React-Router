bf4e28a6c86ed9afe5159d9e12eb182e37c30bf0
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-03-20 13:25:31 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { Link, Routes, Route } from "react-router-dom";
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Blue from './components/Blue';
+import Red from './components/Red';
+import Purple from './components/Purple';
+import Green from './components/Green';
+import './App.css';
 
 function App() {
 
 
   return (
     <>
-     
+     <div id="container">
+      <Nav />     
+      <h2>Hello React Router!</h2>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/blue" element={<Blue />}/>
+          <Route path="/red" element={<Red />}/>
+          <Route path="/purple" element={<Purple />}/>
+          <Route path="/green" element={<Green />}/>
+        </Routes>
+      </div>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return ( 
+        <div id="navbar">
+            <nav style={{ display: "flex", gap: "25px" }}>
+                <Link to="/">Home</Link>
+                <Link to="/blue">Blue</Link>
+                <Link to="/red">Red</Link>
+                <Link to="/purple">Purple</Link>
+                <Link to="green">Green</Link>
+            </nav>
+      </div>
+     );
+}
+ 
+export default Nav;
